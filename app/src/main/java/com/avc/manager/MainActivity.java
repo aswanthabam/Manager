2349ece11cda11b49c57023aa262b29cd3502c08
avc-tech <avc.official@gmail.com>
2022-05-01 14:39:55 +0530

    New prohect AVC Manager

---
+++ app/src/main/java/com/avc/manager/MainActivity.java
@@ -0,0 +1,14 @@
+package com.avc.manager;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
